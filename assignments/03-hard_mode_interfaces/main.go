8e1e8da27fa98fff4d639f367b21b282c0e3715e
Franklin Wu <franklin.w.wu@gmail.com>
2018-04-13 03:00:28 -0700

    Complete assignment 03 code

--- assignments/03-hard_mode_interfaces/main.go
+++ assignments/03-hard_mode_interfaces/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	argc := len(os.Args)
+	if argc != 2 {
+		fmt.Println("usage: <go execution command> <filename>")
+	} else {
+		filename := os.Args[1]
+		fmt.Println("processing", filename, "...")
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			written, err := io.Copy(os.Stdout, file)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("... wrote", written, "bytes")
+			}
+		}
+	}
 }
